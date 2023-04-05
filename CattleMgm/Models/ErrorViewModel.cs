f6bf6f1a29f0b763ce94049a87c6a6c10d6cd04d
Fiton Frangu <fiton.frangu@dataprognet-ks.com>
2023-04-05 09:29:13 +0200

    Add project files.

---
+++ CattleMgm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CattleMgm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
