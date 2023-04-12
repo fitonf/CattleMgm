bef7f86d0dde6fa317e83b937bffa3c1e9a66f9d
Fiton Frangu <fiton.frangu@dataprognet-ks.com>
2023-04-12 15:23:59 +0200

    user create and user index

--- CattleMgm/Controllers/UserController.cs
+++ CattleMgm/Controllers/UserController.cs
@@ -1,14 +1,26 @@
 ﻿using CattleMgm.Data;
 using CattleMgm.Data.Entities;
+using CattleMgm.Models;
 using CattleMgm.ViewModels.User;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CattleMgm.Controllers
 {
     public class UserController : BaseController
     {
-        public UserController(ApplicationDbContext context, praktikadbContext db) : base(context, db)
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public UserController(ApplicationDbContext context, praktikadbContext db, 
+            SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager,
+            RoleManager<ApplicationRole> roleManager) : base(context, db)
         {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         public IActionResult Index()
@@ -44,6 +56,50 @@ namespace CattleMgm.Controllers
                          isRoleConfirmed = item.IsRoleConfirmed == null ? false : item.IsRoleConfirmed.Value,
                      }).ToList();
 
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var roles = _db.AspNetRoles.ToList();
+            ViewBag.Roles = new SelectList(roles, "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(UserCreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Plotesoni fushat obligative");
+                return View(model);
+            }
+
+            var role = await _roleManager.FindByIdAsync(model.RoleId);
+            if (role == null)
+            {
+                ModelState.AddModelError("", "Ky rol nuk ekziston");
+                return View(model);
+            }
+
+            ApplicationUser newUser = Activator.CreateInstance<ApplicationUser>();
+            newUser.FirstName = model.FirstName;
+            newUser.LastName = model.LastName;
+            newUser.Email = model.Email;
+            newUser.UserName = model.UserName;
+            newUser.PhoneNumber = model.PhoneNumber;
+
+            var result = await _userManager.CreateAsync(newUser, model.Password);
+
+            if(result.Succeeded)
+            {
+                result = await _userManager.AddToRoleAsync(newUser, role.Name);
+                return RedirectToAction("Index", "User");
+            }
+
+            //ka ndodh nje gabim
             return View(model);
         }
     }
--- CattleMgm/ViewModels/User/UserViewModel.cs
+++ CattleMgm/ViewModels/User/UserViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace CattleMgm.ViewModels.User
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CattleMgm.ViewModels.User
 {
     public class UserViewModel
     {
@@ -15,5 +17,38 @@
         public string LastName { get; set; }
 
         public bool isRoleConfirmed { get; set; }
+    }
+
+    public class UserCreateViewModel
+    {
+        [Required(ErrorMessage = "Kjo fushë eshte obligative")]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Kjo fushë eshte obligative")]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Kjo fushë eshte obligative")]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Kjo fushë eshte obligative")]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+        [Display(Name = "Phone number")]
+        public string? PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Kjo fushë është obligative")]
+        public string RoleId { get; set; }
+
+        [Required(ErrorMessage = "Kjo fushë është obligative")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Fjalekalimet ndryshojne")]
+        public string ConfirmPassword { get; set; }
     }
 }
