959a02eac0a5ad797f5ec3e69031a47d089c9d24
gent.govori <gent.govori@GENTGOVORI>
2023-04-13 12:01:36 +0200

    Edit i user

--- CattleMgm/Controllers/UserController.cs
+++ CattleMgm/Controllers/UserController.cs
@@ -1,10 +1,13 @@
 ﻿using CattleMgm.Data;
 using CattleMgm.Data.Entities;
 using CattleMgm.Models;
+using CattleMgm.Repository.Cattles;
 using CattleMgm.ViewModels.User;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace CattleMgm.Controllers
 {
@@ -12,7 +15,8 @@ namespace CattleMgm.Controllers
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly RoleManager<ApplicationRole> _roleManager; 
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        
 
         public UserController(ApplicationDbContext context, praktikadbContext db, 
             SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager,
@@ -21,6 +25,7 @@ namespace CattleMgm.Controllers
             _signInManager = signInManager;
             _userManager = userManager;
             _roleManager = roleManager;
+            
         }
 
         public IActionResult Index()
@@ -84,23 +89,138 @@ namespace CattleMgm.Controllers
                 return View(model);
             }
 
-            ApplicationUser newUser = Activator.CreateInstance<ApplicationUser>();
+            ApplicationUser newUser = Activator.CreateInstance<ApplicationUser>(); //new ApplicationUser();
             newUser.FirstName = model.FirstName;
             newUser.LastName = model.LastName;
             newUser.Email = model.Email;
             newUser.UserName = model.UserName;
             newUser.PhoneNumber = model.PhoneNumber;
+            //newUser.RoleId = model.RoleId;
 
             var result = await _userManager.CreateAsync(newUser, model.Password);
 
             if(result.Succeeded)
             {
-                result = await _userManager.AddToRoleAsync(newUser, role.Name);
+                result = await _userManager.AddToRoleAsync(newUser, role.Name);//e ben insertimin e rolit ne databaze
                 return RedirectToAction("Index", "User");
             }
 
             //ka ndodh nje gabim
             return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditAsync(string id)
+        {
+           
+            if (String.IsNullOrEmpty(id))
+            {
+                ModelState.AddModelError("", "Id eshte e zbrazet");
+                return View();
+            }
+            var user = await _userManager.FindByIdAsync(id);
+
+            if(user ==null)
+            {
+                ModelState.AddModelError("", "Ky user nuk ekziston");
+                return View();
+            }
+
+            var roles = _db.AspNetRoles.ToList();
+            ViewBag.Roles = new SelectList(roles, "Id", "Name");
+
+            UserEditViewModel model = new UserEditViewModel();
+            model.Id = user.Id;
+            model.FirstName = user.FirstName;
+            model.LastName = user.LastName;
+            model.Email = user.Email;
+            model.PhoneNumber = user.PhoneNumber;
+            model.UserName = user.UserName;
+
+            var roleName = await _userManager.GetRolesAsync(user);
+            //gjetja e rolit sipas emrit
+            var roleUser = await _roleManager.FindByNameAsync(roleName.First());
+
+            model.RoleId = roleUser.Id;
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditAsync(UserEditViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Plotesoni fushat obligative");
+                return View(model);
+            }
+
+            //
+            var user = await _userManager.FindByIdAsync(model.Id);
+
+            if(user ==null)
+            {
+                ModelState.AddModelError("", "Ky user nuk ekziston");
+                return View(model);
+            }
+
+            //ndryshimi i te dhenave perveq rolit nga model ne db 
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            user.Email = model.Email;
+            user.PhoneNumber = model.PhoneNumber;
+            user.UserName = model.UserName;
+
+            //gjetja e rolit 
+            var role = await _roleManager.FindByIdAsync(user.RoleId);
+            if (role == null)
+            {
+                ModelState.AddModelError("", "Ky rol nuk ekziston");
+                return View(model);
+            }
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var rolename = roles.First();
+            
+            var oldRole = await _userManager.RemoveFromRoleAsync(user, rolename);
+
+            var newRole = await _roleManager.FindByIdAsync(model.RoleId);
+
+            var roleUpdated =await  _userManager.AddToRoleAsync(user, newRole.Name);
+
+            var result = await _userManager.UpdateAsync(user);
+
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index", "User");
+            }
+
+            //ka ndodh nje gabim
+            return View(model);
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                //ModelState.AddModelError("", "");
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Json("Kjo id nuk ekziston");
+            }
+
+            //gjetja e user-it
+            var user = await _userManager.FindByIdAsync(id);
+            if (user is not null)
+            {
+                var result = await _userManager.DeleteAsync(user);
+            }
+
+            return Json("success");
+
         }
     }
 }
--- CattleMgm/ViewModels/User/UserViewModel.cs
+++ CattleMgm/ViewModels/User/UserViewModel.cs
@@ -51,4 +51,39 @@ namespace CattleMgm.ViewModels.User
         [Compare("Password", ErrorMessage = "Fjalekalimet ndryshojne")]
         public string ConfirmPassword { get; set; }
     }
+
+    public class UserEditViewModel
+    {
+        public string Id { get; set; }
+
+		[Required(ErrorMessage = "Kjo fushë eshte obligative")]
+		[Display(Name = "First name")]
+		public string FirstName { get; set; }
+
+		[Required(ErrorMessage = "Kjo fushë eshte obligative")]
+		[Display(Name = "Last name")]
+		public string LastName { get; set; }
+
+		[Required(ErrorMessage = "Kjo fushë eshte obligative")]
+		[EmailAddress]
+		[Display(Name = "Email")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Kjo fushë eshte obligative")]
+		[Display(Name = "Username")]
+		public string UserName { get; set; }
+		[Display(Name = "Phone number")]
+		public string? PhoneNumber { get; set; }
+
+		[Required(ErrorMessage = "Kjo fushë është obligative")]
+		public string RoleId { get; set; }
+
+		[Required(ErrorMessage = "Kjo fushë është obligative")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[DataType(DataType.Password)]
+		[Compare("Password", ErrorMessage = "Fjalekalimet ndryshojne")]
+		public string ConfirmPassword { get; set; }
+	}
 }
