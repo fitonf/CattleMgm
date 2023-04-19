5655848db95a858465e8cf47dda584f3396e2139
adhurimhalili <ah51755@ubt-uni.net>
2023-04-19 13:38:08 +0200

    Created initial Roles List

--- CattleMgm/Controllers/RolesController.cs
+++ CattleMgm/Controllers/RolesController.cs
@@ -4,19 +4,66 @@ using CattleMgm.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using CattleMgm.ViewModels.Roles;
 
 namespace CattleMgm.Controllers
 {
-    [AllowAnonymous]
+    [Authorize]
     public class RolesController : BaseController
     {
-        public RolesController(ApplicationDbContext context, praktikadbContext db, UserManager<ApplicationUser> userManager) : base(context, db, userManager)
+        public readonly RoleManager<ApplicationRole> roleManager;
+
+
+        public RolesController(ApplicationDbContext context, praktikadbContext db,
+            UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager) : base(context, db, userManager)
         {
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var roles = _context.Roles.ToList();
+            List<RolesViewModel> model = new List<RolesViewModel>();
+
+
+            model = (from item in roles
+                     select new RolesViewModel
+                     {
+                         Id = item.Id,
+                         Name = item.Name
+                     }).ToList();                
+
+            return View(model);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(RolesCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // We just need to specify a unique role name to create a new role
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.Name
+                };
+
+                // Saves the role in the underlying AspNetRoles table
+                IdentityResult result = await roleManager.CreateAsync((ApplicationRole)identityRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Roles");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+
+        }
+
     }
 }
--- CattleMgm/ViewModels/Roles/RolesViewModel.cs
+++ CattleMgm/ViewModels/Roles/RolesViewModel.cs
@@ -1,6 +1,25 @@
-﻿namespace CattleMgm.ViewModels.Roles
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CattleMgm.ViewModels.Roles
 {
     public class RolesViewModel
     {
+        
+        [Required]
+        [Display(Name = "Emri")]
+        public string Name { get; set; }
+        public string Id { get; set; }
+    }
+
+    public class RolesCreateViewModel
+    {
+        [Required]
+        [Display(Name = "Emri")]
+        public string Name { get; set; }
+    }
+
+    public class RolesEditViewModel
+    {
+        public string Id { get; set; }
     }
 }
