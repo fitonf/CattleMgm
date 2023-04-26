9367ae3ed5624f6c54184d460aa77f22852b685c
Gentrit Arifi <gentritarifi2000@gmail.com>
2023-04-26 09:50:50 +0200

    Krijimi i formes per editim, regjistrim dhe fshirjen e lageshtise.

---
+++ CattleMgm/Repository/Humidity/IHumidityRepository.cs
@@ -0,0 +1,12 @@
+﻿using CattleMgm.Data.Entities;
+
+namespace CattleMgm.Repository.Humidity
+{
+    public interface IHumidityRepository
+    {
+        //Task<List<Data.Entities.CattleHumidity>> CattleHumidity();
+        Task<List<Data.Entities.CattleHumidity>> GetHumidity();
+
+    }
+}
+    
