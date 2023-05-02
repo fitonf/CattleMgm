a999d4025ceee397b95fa4b94b3363f39b257c90
MuratB17 <90219498+MuratB17@users.noreply.github.com>
2023-05-02 15:35:53 +0200

    Breed Report

---
+++ CattleMgm/Models/BreedModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CattleMgm.Models
+{
+    public class BreedReportModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int? Type { get; set; }
+    }
+}
