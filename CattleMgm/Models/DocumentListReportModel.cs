a2f479801d22355845ed01d52746bd1ecf8ae496
Blendi <blendibs2000@gmail.com>
2023-05-03 16:10:54 +0200

    butoni per shkarkim te dokumenteve

---
+++ CattleMgm/Models/DocumentListReportModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CattleMgm.Models
+{
+    public class DocumentListReportModel
+    {
+        public string Name { get; set; }
+        public int Type { get; set; }
+        public DateTime Created { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
