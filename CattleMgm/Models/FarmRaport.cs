abe85374145eaf50815a4aa05ea14d3ca7496562
Milot Azemi <milotazemi5@gmail.com>
2023-05-04 15:41:30 +0200

    Farm-Raport

---
+++ CattleMgm/Models/FarmRaport.cs
@@ -0,0 +1,22 @@
+﻿namespace CattleMgm.Models
+{
+    public class FarmModel
+    {
+        public int Id { get; set; }
+        public string FarmName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+        public string Addres { get; set; } = string.Empty;
+
+        public bool Notification { get; set; }
+        public bool AdminNotification { get; set; }
+        public string? ImageProfile { get; set; }
+    }
+
+    public class FarmRaportModel
+    {
+        public int Id { get; set; }
+        public string FarmName { get; set; }
+        public string Place { get; set; }
+        public string Addres { get; set; }
+    }
+}
