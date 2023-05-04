953a87ff5711ed5fe9ffe8a29e6ec10bfe291e95
Blendi <blendibs2000@gmail.com>
2023-05-04 17:48:11 +0200

    search dhe download

--- CattleMgm/Controllers/MediaController.cs
+++ CattleMgm/Controllers/MediaController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using CattleMgm.ViewModels.Media;
 using CattleMgm.Repository.Media;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using CattleMgm.Models.Session;
 
 namespace CattleMgm.Controllers
 {
@@ -65,6 +66,34 @@ namespace CattleMgm.Controllers
 
             return View(listaViewModel);
         }
+        public async Task<IActionResult> Lista()
+        {
+            return View();
+        }
+        public async Task<IActionResult> _Lista(SearchMediaModel model)
+        {
+            var lista = await _mediaRepository.GetAllFiles();
+
+            List<MediaListViewModel> listaViewModel = new List<MediaListViewModel>();
+
+            foreach (var media in lista)
+            {
+                listaViewModel.Add(new MediaListViewModel
+                {
+                    id = media.id,
+                    Name = media.Name,
+                    Identifier = media.Identifier,
+                    Path = media.Path,
+                    Type = media.Type,
+                    Created = media.Created,
+                    CreatedBy = media.CreatedBy,
+                }); ;
+            }
+
+            listaViewModel = listaViewModel.OrderByDescending(q => q.Name).ToList();
+
+            return Json(listaViewModel);
+        }
 
 
         [HttpPost]
@@ -99,5 +128,28 @@ namespace CattleMgm.Controllers
             return RedirectToAction("DocumentList");
         }
 
+        #region Report
+
+        [HttpPost]
+        public async Task<JsonResult> OpenIndexReport()
+        {
+            var media = _db.Media.ToList();
+            var query = media
+               .Select(q => new DocumentListReportModel
+               {
+                   Name = q.Name,
+                   Type = q.Type,
+                   Created = q.Created,
+                   CreatedBy = _db.AspNetUsers.Where(t => t.Id == q.CreatedBy).Count() > 0 ? _db.AspNetUsers.Where(t => t.Id == q.CreatedBy).FirstOrDefault().UserName : "",
+               }).ToList();
+
+
+            HttpContext.Session.SetString("Path", "Reports\\MediaReport.rdl");
+            HttpContext.Session.Set("queryresult", query);
+
+
+            return Json(true);
+        }
+        #endregion
     }
 }
---
+++ CattleMgm/Models/MediaModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CattleMgm.Models
+{
+    public class SearchMediaModel
+    {
+        public string Name { get; set; }
+        public int Type { get; set; }
+    }
+}
